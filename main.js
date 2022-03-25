a850641d3739372febaafea8bbeba9dddd30445f
Tan Nuc <nguyenngoctan130695@gmail.com>
2022-03-25 12:54:28 +0700

    Edit index.html

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+// Reference: https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button
 function displayRadioValue() {
     var ele = document.getElementsByName('radioRating');
       
     for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        document.getElementById("selectedRating").innerHTML
+        if(ele[i].checked){
+            document.getElementById("selectedRating").innerHTML
                 = `You selected ${ele[i].value} out of 5`;
+        }
+        
     }
 }
 // Submit Button
