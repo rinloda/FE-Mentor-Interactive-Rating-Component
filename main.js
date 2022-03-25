c0b7473eb808579769a58a0262ee555b86a869e7
Tan Nuc <nguyenngoctan130695@gmail.com>
2022-03-25 12:20:12 +0700

    edit main.js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const button = document.querySelector('input');
-
+function displayRadioValue() {
+    var ele = document.getElementsByName('radioRating');
+      
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+        document.getElementById("selectedRating").innerHTML
+                = `You selected ${ele[i].value} out of 5`;
+    }
+}
+// Submit Button
 const submitRating = document.querySelector('#submit-rating');
 const submitButton = document.querySelector('#submit-button');
 const respondRating = document.querySelector('#response-rating');
-submitButton.addEventListener('click', generateThankyou);
-
 function generateThankyou(){
     submitRating.classList.add('hidden');
     respondRating.classList.remove('hidden');
 }
+
+
+
+
+submitButton.addEventListener('click', ()=> {
+    displayRadioValue();
+    generateThankyou();
+} );
+
